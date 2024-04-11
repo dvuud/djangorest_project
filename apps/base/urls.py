e2a780ce68e9da734889557906f575c98be2fe59
dvuud <baltabaevdavud88@gmail.com>
2024-04-11 19:09:05 +0600

    updates

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('product_api', ProductAPIView.as_view(), name='api_products'),
-    path('product_api', ProductAPI.as_view(), name='products'),
-    path('product_api/<int:pk>/', ProductDetail.as_view(), name='products_detail')
+    path('product_api_detail', ProductAPI.as_view(), name='products'),
+    path('product_api_create/<int:pk>/', ProductDetail.as_view(), name='products_detail')
     
 ]
